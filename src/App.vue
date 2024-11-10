91250cc7ea4a47943bf075d5a3addf55a17858d0
Valid Magomadov <100527555+dgssdagdg@users.noreply.github.com>
2024-11-10 17:51:53 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h1>Добро пожаловать в мой Telegram WebApp!</h1>
+    <button @click="onButtonClick">Нажми на меня!</button>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  mounted() {
+    const tg = window.Telegram.WebApp;
+    tg.ready(); // Убедитесь, что WebApp API готов
+    console.log('User data:', tg.initDataUnsafe);
+  },
+  methods: {
+    onButtonClick() {
+      alert('Кнопка нажата!');
+    },
+  },
+};
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss"scoped>
+/* Ваши стили */
 </style>
